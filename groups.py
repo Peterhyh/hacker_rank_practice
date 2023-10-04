06d80768c46112bf8cd536b8f53bbd1ff0c10e8e
Peter Huynh <peterhyh@yahoo.com>
2023-10-03 20:39:13 -0500

    replace all the && and || in a string

--- groups.py
+++ groups.py
@@ -11,6 +11,11 @@
 
 
 
+
+
+
+
+
 # import re
 
 # expression=r'(?<=[^aeiouAEIOU0-9+-])[aeiouAEIOU]{2,}(?=[^aeiouAEIOU0-9+-])'
@@ -26,23 +31,46 @@
 # else:
 #     print(-1)
 
+
+
+
+
+
+
+
+
+
+
+# import re
+
+# string = 'aaadaa'
+# combo = 'aa'
+# expression = r'(?=('+ combo +'))'
+# matches = re.finditer(expression, string)
+
+# anymatch=False
+
+# for match in matches:
+#     anymatch = True
+#     print((match.start(1), match.end(1)-1))
+
+
+# if matches == False:
+#     print((-1,-1))
+
+
+
 import re
 
-string = 'aaadaa'
-combo = 'aa'
-expression = r'(?=('+ combo +'))'
-matches = re.finditer(expression, string)
+lines = 11
+text = "a = 1;\nb = input();\n\nif a + b > 0 && a - b < 0:\nstart()\nelif a*b > 10 || a/b < 1:\nstop()\nprint set(list(a)) | set(list(b)) \nNote do not change &&& or ||| or & or |\nOnly change those '&&' which have space on both sides.\nOnly change those '|| which have space on both sides.)"
 
 
+def change(number):
+    if number.group(1) == '&&':
+        return 'and'
+    else:
+        return 'or'
 
-
-anymatch=False
-
-for match in matches:
-    anymatch = True
-    print((match.start(1), match.end(1)-1))
-
-
-if matches == False:
-    print((-1,-1))
-
+pattern = r'(?<= )(\|\||&&)(?= )'
+print(re.sub(pattern, change, text ))
