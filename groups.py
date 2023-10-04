17a6baf740a3483e5f430f254e3e3cbccd2de6f8
Peter Huynh <peterhyh@yahoo.com>
2023-10-03 19:54:08 -0500

    print index start and end for matching sets

--- groups.py
+++ groups.py
@@ -11,18 +11,38 @@
 
 
 
+# import re
+
+# expression=r'(?<=[^aeiouAEIOU0-9+-])[aeiouAEIOU]{2,}(?=[^aeiouAEIOU0-9+-])'
+
+# string='rabcdeefgyYhFjkIoomnpOeorteeeeet'
+
+# matches = re.findall(expression, string)
+
+
+# if matches:
+#     for match in matches:
+#         print(match)
+# else:
+#     print(-1)
+
 import re
 
-expression=r'(?<=[^aeiouAEIOU0-9+-])[aeiouAEIOU]{2,}(?=[^aeiouAEIOU0-9+-])'
+string = 'aaadaa'
+combo = 'aa'
+expression = r'(?=('+ combo +'))'
+matches = re.finditer(expression, string)
 
-string='rabcdeefgyYhFjkIoomnpOeorteeeeet'
-
-matches = re.findall(expression, string)
 
 
-if matches:
-    for match in matches:
-        print(match)
-else:
-    print(-1)
+
+anymatch=False
+
+for match in matches:
+    anymatch = True
+    print((match.start(1), match.end(1)-1))
+
+
+if matches == False:
+    print((-1,-1))
 
