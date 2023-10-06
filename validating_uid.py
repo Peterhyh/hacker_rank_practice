322b70ed85d0c25822728da4192e6ac5198a6ed4
Peter Huynh <peterhyh@yahoo.com>
2023-10-06 11:58:40 -0500

    validating uid answer

--- validating_uid.py
+++ validating_uid.py
@@ -1,20 +1,46 @@
 import re
 
-x = 'B1CD102354'
+x = 'B1cD102354'
 
 uppercase_pattern = r'[A-Z]'
-
+number_pattern = r'[0-9]'
+alphanumeric_pattern = r'^[a-zA-Z0-9]*$'
+max_count_pattern = r'{10}'
 
 class IdCheck():
-    def check_upper(self, id):
+    def uppercase(self, id):
         upper_count = len(re.findall(uppercase_pattern, id))
         if upper_count >= 2:
             return True
+        else:
+            return False
 
+    def number(self, id):
+        number_count = len(re.findall(number_pattern, id))
+        if number_count >= 3:
+            return True
+        else:
+            return False
 
+        
+    def validity(self, id):
+        alphanumeric = bool(re.match(alphanumeric_pattern, id))
+        count = bool(re.match(max_count_pattern, id))
+        
+        id_char = set()
+        for char in id_char:
+            if char in id_char:
+                valid_char = False
+            id_char.add(char)
 
+        if alphanumeric == True and count == True and valid_char == True:
+            return True
+        else:
+            return False
+        
 
 check = IdCheck()
 
 
-print(check.check_upper(x))
+
+
