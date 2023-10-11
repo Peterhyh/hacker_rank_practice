6de397483df019fe176d2bd2f2bbdfdbd2a25c4d
Peter Huynh <peterhyh@yahoo.com>
2023-10-11 16:52:43 -0500

    comments deleted on validate zip

--- regex_validate_zip.py
+++ regex_validate_zip.py
@@ -1,5 +1,5 @@
-regex_integer_in_range = r"^([1-9][0-9]{5})$"	# Do not delete 'r'.
-regex_alternating_repetitive_digit_pair = r"(?=(.)(.)(\1))"	# Do not delete 'r'.
+regex_integer_in_range = r"^([1-9][0-9]{5})$"
+regex_alternating_repetitive_digit_pair = r"(?=(.)(.)(\1))"
 
 
 
