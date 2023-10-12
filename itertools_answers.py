eb0eeada2602d8bd54fe53736a2b52b0aa1777ff
Peter Huynh <peterhyh@yahoo.com>
2023-10-12 16:43:41 -0500

    itertools part 1 complete

---
+++ itertools_answers.py
@@ -0,0 +1,10 @@
+#print all the combinations possible with the given string and max number of characters
+# from itertools import combinations
+
+# test_string = 'HACK 2'
+# string_maxnum = test_string.split(' ')
+# string = string_maxnum[0]
+# max_num = string_maxnum[1]
+# for i in range(1, (int(max_num) + 1)):
+#     for combo in combinations(sorted(string), i):
+#         print(''.join(combo))
